5fbe83006eb28e9de828743916d787a1c871fcee
Josh Brown <39962766+jbrown1234@users.noreply.github.com>
2019-07-05 16:14:12 -0400

    Added copyright notes.
    Added copyright notes.

--- Instrument_Examples/Instructables/Get_Started_with_Instr_Control_Go/user_functions.lua
+++ Instrument_Examples/Instructables/Get_Started_with_Instr_Control_Go/user_functions.lua
@@ -1,3 +1,11 @@
+--[[
+
+***********************************************************
+*** Copyright Tektronix, Inc.                      ***
+*** See www.tek.com/sample-license for licensing terms. ***
+***********************************************************
+
+]]
 function do_beep(duration, frequency)
     beeper.beep(duration, frequency)
 end
