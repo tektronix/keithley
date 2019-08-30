ba4076c0e0f2d3c636d1d3738420c46dd3f3c584
Josh Brown <39962766+jbrown1234@users.noreply.github.com>
2019-08-30 09:25:06 -0400

    Comments updated
    The function purpose was ill-defined for the write, read, and query operations. Fixed.

--- Instrument_Examples/DAQ6510/Scanning_Low_Level_DC_Voltage/DAQ6510_Scanning_Low_Level_DC_Voltage_SCPI.py
+++ Instrument_Examples/DAQ6510/Scanning_Low_Level_DC_Voltage/DAQ6510_Scanning_Low_Level_DC_Voltage_SCPI.py
@@ -65,7 +65,7 @@ def instrument_disconnect(my_socket):
 """*********************************************************************************
 	Function: instrument_write(my_socket, my_command)
 	
-	Purpose: Issue controlling commands to the target instrument.
+	Purpose: This function issues control commands to the target instrument.
 
 	Parameters:
 		my_socket - The TCP instrument connection object used for sending
@@ -89,8 +89,9 @@ def instrument_write(my_socket, my_command):
 """*********************************************************************************
 	Function: instrument_read(my_socket, receive_size)
 	
-	Purpose: Break the LAN/Ethernet connection between the controlling computer
-			 and the target instrument.
+	Purpose: This function asks the connected instrument to reply with some
+                 previously requested information, typically queued up from a call
+                 to instrument_write().
 
 	Parameters:
 		my_socket - The TCP instrument connection object used for sending
@@ -112,14 +113,18 @@ def instrument_read(my_socket, receive_size):
 """*********************************************************************************
 	Function: instrument_query(my_socket, my_command, receive_size)
 	
-	Purpose: Break the LAN/Ethernet connection between the controlling computer
-		 and the target instrument.
+	Purpose: This function issues control commands to the target instrument with
+                 the expectation that data will be returned. For this function
+                 instance, the returned data is (typically) in string format. 
 
 	Parameters:
 		my_socket - The TCP instrument connection object used for sending
 			    and receiving data.
 		my_command (string) - The command issued to the instrument to make it 
-				      perform some action or service. 
+				      perform some action or service.
+		receive_size (int) - The approximate number of bytes of data the caller
+                                     expects to be returned in the response from the
+                                     instrument. 
 	Returns:
 		reply_string (string) - The requested information returned from the 
 					target instrument. Obtained by way of a caller
