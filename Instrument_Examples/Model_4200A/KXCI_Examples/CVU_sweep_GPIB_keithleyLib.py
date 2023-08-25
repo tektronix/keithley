5ca8fbfaa7634a03cba6feb3d4597f279f9bc822
Brown <josh.brown@keithley.com>
2023-08-25 10:14:27 -0400

    Updates to Account for 4200A Comms Uniqueness
    Corrections to the way that the code uses terminators as well as queries for many to all comms commands to addresss the ACK that's part of a command response.

--- Instrument_Examples/Model_4200A/KXCI_Examples/CVU_sweep_GPIB_keithleyLib.py
+++ Instrument_Examples/Model_4200A/KXCI_Examples/CVU_sweep_GPIB_keithleyLib.py
@@ -10,14 +10,15 @@
     be plotted.
 """
 
-import time
 from instrcomms import Communications
 import csv
 
-INST_RESOURCE_STR = "GPIB0::17::INSTR" # instrument resource string, obtained from NI-VISA Interactive
-my4200 = Communications(INST_RESOURCE_STR) # opens the resource manager in PyVISA with the corresponding instrument resource string
-my4200.connect() # opens connections to the 4200A-SCS
+INST_RESOURCE_STR = "GPIB0::17::INSTR"  # instrument resource string, obtained from NI-VISA Interactive
+my4200 = Communications(INST_RESOURCE_STR)  # opens the resource manager in PyVISA with the corresponding instrument resource string
 
+my4200.connect()  # opens connections to the 4200A-SCS
+
+my4200.write("BC")
 my4200.write("DR1")
 my4200.write(":CVU:RESET")
 my4200.write(":CVU:MODE 1")
@@ -29,30 +30,28 @@ my4200.write(":CVU:SWEEP:DCV 5, -5, -0.2")
 my4200.write(":CVU:DELAY:SWEEP 0.1")
 my4200.write(":CVU:TEST:RUN")
 
-time.sleep(20) # time needed for test to finish
+my4200._instrument_object.wait_for_srq()  # waits until data is ready by waiting for serial request coming from the 4200A-SCS
 
-CpGp = my4200.query(":CVU:DATA:Z?") # queries readings of Cp-Gp
-Volt = my4200.query(":CVU:DATA:VOLT?") # queries readings of Voltage
+CpGp = my4200.query(":CVU:DATA:Z?")  # queries readings of Cp-Gp
+Volt = my4200.query(":CVU:DATA:VOLT?")  # queries readings of Voltage
 
-sep = ';' # separator between Cp-Gp
+sep = ";"  # separator between Cp-Gp
 
-VoltList = Volt.split(',') # splits voltage list at commas
-CpGpList = CpGp.split(',') # splits Cp-Gp list at commas
+VoltList = Volt.split(",")  # splits voltage list at commas
+CpGpList = CpGp.split(",")  # splits Cp-Gp list at commas
 
-CapList = [] # list only for capacitance values
+CapList = []  # list only for capacitance values
 
-for z in CpGpList: # separates the Cp-Gp list into only the Cp values, by removing the value after the semi-colon
+for (z) in (CpGpList):  # separates the Cp-Gp list into only the Cp values, by removing the value after the semi-colon
     CapList.append(z.split(sep, 1)[0])
 
-zipped_list = zip(VoltList, CapList) # creates iterable zipped list
+zipped_list = zip(VoltList, CapList)  # creates iterable zipped list
 
-columns = ['Voltage (V)', 'Capacitance (F)']  # column headers
-with open('output.csv', 'w', newline='', encoding='utf-8') as f: # opens and writes csv file
-    writer =  csv.writer(f)
+columns = ["Voltage (V)", "Capacitance (F)"]  # column headers
+with open("output.csv", "w", newline="", encoding="utf-8") as f:  # opens and writes csv file
+    writer = csv.writer(f)
     writer.writerow(columns)
     writer.writerows(zipped_list)
 
-f.close() # close stream
-my4200.disconnect() # close communications with the 4200A-SCS
-
-
+f.close()  # close stream
+my4200.disconnect()  # close communications with the 4200A-SCS
--- Instrument_Examples/Model_4200A/KXCI_Examples/family_curves_LAN_keithleyLib.py
+++ Instrument_Examples/Model_4200A/KXCI_Examples/family_curves_LAN_keithleyLib.py
@@ -13,41 +13,41 @@
 import time
 from instrcomms import Communications
 
-term = '\0' # terminating character for ethernet commands
-INST_RESOURCE_STR = "TCPIP0::169.254.88.169::1225::SOCKET" # instrument resource string, obtained from NI MAX
+INST_RESOURCE_STR = "TCPIP0::169.254.160.161::1225::SOCKET"  # instrument resource string, obtained from NI MAX
+my4200 = Communications(INST_RESOURCE_STR)  # opens the resource manager in PyVISA with the corresponding instrument resource string
 
-my4200 = Communications(INST_RESOURCE_STR) # opens the resource manager in PyVISA with the corresponding instrument resource string
-my4200.connect() # opens connections to the 4200A-SCS
+my4200.connect()  # opens connections to the 4200A-SCS
+my4200._instrument_object.write_termination = "\0"  # Set PyVISA write terminator
+my4200._instrument_object.read_termination = "\0"  # Set PyVISA read terminator
 
-my4200.write("DE"+term) 
-my4200.write("CH1, 'VS', 'IS', 1, 3"+term) 
-my4200.write("CH2, 'VD', 'ID', 1, 1"+term) 
-my4200.write("CH3, 'VG', 'IG', 1, 2"+term) 
-my4200.write("SS"+term) 
-my4200.write("VR1, 0, 5, 0.1, 100e-3"+term) 
-my4200.write("VP2, 1, 4, 100e-3"+term) 
-my4200.write("VC1, 0, 100e-3"+term)
-my4200.write("HT 0"+term) 
-my4200.write("DT 0.001"+term) 
-my4200.write("IT2"+term) 
-my4200.write("RS 5"+term) 
-my4200.write("RG 1, 100e-9"+term) 
-my4200.write("RG 2, 100e-9"+term) 
-my4200.write("RG 3, 100e-9"+term) 
-my4200.write("SM"+term) 
-my4200.write("DM1"+term) 
-my4200.write("XN 'VD', 1, 0, 5"+term) 
-my4200.write("YA 'ID', 1, 0, 0.04"+term) 
-my4200.write("MD"+term)
-my4200.write("ME1"+term) 
+my4200.query("BC")
+my4200.query("DE")
+my4200.query("CH1, 'VS', 'IS', 1, 3")
+my4200.query("CH2, 'VD', 'ID', 1, 1")
+my4200.query("CH3, 'VG', 'IG', 1, 2")
+my4200.query("SS")
+my4200.query("VR1, 0, 5, 0.1, 100e-3")
+my4200.query("VP2, 1, 4, 100e-3")
+my4200.query("VC1, 0, 100e-3")
+my4200.query("HT 0")
+my4200.query("DT 0.001")
+my4200.query("IT2")
+my4200.query("RS 5")
+my4200.query("RG 1, 100e-9")
+my4200.query("RG 2, 100e-9")
+my4200.query("RG 3, 100e-9")
+my4200.query("SM")
+my4200.query("DM1")
+my4200.query("XN 'VD', 1, 0, 5")
+my4200.query("YA 'ID', 1, 0, 0.04")
+my4200.query("MD")
+my4200.query("ME1")
 
 # wait for measurement to complete
-status = my4200.query("SP"+term)
+status = my4200.query("SP")
 
 while int(status) != 1:
-    status = my4200.query("SP"+term)
+    status = my4200.query("SP")
     time.sleep(1)
 
-my4200.disconnect() # close communications with the 4200A-SCS
-
-
+my4200.disconnect()  # close communications with the 4200A-SCS
